e22494b666d9218e07c791eb730c143c48ed6e77
vinayupadhyayula <vupadhyayula@binghamton.edu>
2024-02-11 18:57:38 -0500

    changes

--- kaizntree_back_end/settings.py
+++ kaizntree_back_end/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     'kaizntree',
     'corsheaders'
 ]
