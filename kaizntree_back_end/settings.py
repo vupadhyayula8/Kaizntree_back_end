11cb82b77e29b84961faea0ed50484d6cf94c090
vinayupadhyayula <vupadhyayula@binghamton.edu>
2024-02-11 18:56:33 -0500

    no static url needed

--- kaizntree_back_end/settings.py
+++ kaizntree_back_end/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
