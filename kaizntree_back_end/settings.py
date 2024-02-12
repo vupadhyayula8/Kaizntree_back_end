a4ff11f17f61f1af0cf53e5d61bd810643bfaa0b
vinayupadhyayula <vupadhyayula@binghamton.edu>
2024-02-11 20:24:12 -0500

    minor fix

--- kaizntree_back_end/settings.py
+++ kaizntree_back_end/settings.py
@@ -148,6 +148,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 CORS_ORIGIN_ALLOW_ALL = True
 
+ALLOWED_HOSTS = ['*']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
